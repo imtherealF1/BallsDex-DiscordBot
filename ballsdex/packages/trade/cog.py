7f15e6a5531ce2d9336a6525a7880bcf8fe32565
imtherealF1 <vasilmichas@gmail.com>
2024-11-12 19:00:34 +0200

    adding special arg to trade history

--- ballsdex/packages/trade/cog.py
+++ ballsdex/packages/trade/cog.py
@@ -359,6 +359,7 @@ class Trade(commands.GroupCog):
         trade_user: discord.User | None = None,
         days: Optional[int] = None,
         countryball: BallEnabledTransform | None = None,
+        special: SpecialEnabledTransform | None = None,
     ):
         """
         Show the history of your trades.
@@ -373,6 +374,8 @@ class Trade(commands.GroupCog):
             Retrieve trade history from last x days.
         countryball: BallEnabledTransform | None
             The countryball you want to filter the trade history by.
+        special: SpecialEnabledTransform | None
+            The special you want to filter the trade history by.
         """
         await interaction.response.defer(ephemeral=True, thinking=True)
         user = interaction.user
@@ -400,9 +403,14 @@ class Trade(commands.GroupCog):
 
         if countryball:
             queryset = queryset.filter(Q(tradeobjects__ballinstance__ball=countryball)).distinct()
+        if special:
+            queryset = queryset.filter(Q(tradeobjects__ballinstance__special=special)).distinct()
 
         history = await queryset.order_by(sorting.value).prefetch_related(
-            "player1", "player2", "tradeobjects__ballinstance__ball"
+            "player1",
+            "player2",
+            "tradeobjects__ballinstance__ball",
+            "tradeobjects__ballinstance__special",
         )
 
         if not history:
